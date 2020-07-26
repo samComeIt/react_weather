4463f24cfd305980e07bf681a21d663dd97d2245
samComeIt <sanhae1996@gmail.com>
2020-07-26 17:19:19 +0900

    show date

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+console.log(process.env.REACT_APP_API_KEY)
+
+const api = {
+  key : process.env.REACT_APP_API_KEY,
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
 
 function App() {
+  const dateBuilder = (d) => {
+  let months = ["January", "February", "March", "April", "May", "June", "July","August", "October", "November", "December"];
+  
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div clasName="App">
+      <main>
+      <div className="date">{dateBuilder(new Date())}</div>
+
+      </main>
     </div>
   );
 }
